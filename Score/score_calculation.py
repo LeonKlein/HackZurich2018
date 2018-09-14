d606460b82539cb517416ecab5fdad2a7d33d859
LeonKlein <leon.klein.2601@gmail.com>
2018-09-15 01:54:04 +0200

    Implement loading of Environmental Data

--- Score/score_calculation.py
+++ Score/score_calculation.py
@@ -1,24 +1,35 @@
 import json
 import csv
-
+import re
 
 fname = "DataScrapper/tools/testData.txt"
 fref = "Score/EnvironmentalData.csv"
 
 with open(fname) as f:
     content = f.read()
-    data = json.loads(content)[1]
-    print(data)
-
-#get number of servings
-servings = data['Servings']
+    recipe = json.loads(content)[1]
 
 #get ingredients and their amount
-ingredients = data['Ingredients']
+ingredients = recipe['Ingredients']  
 print(ingredients)
+
+pattern = r'(?P<quantity>[0-9] [a-z]+) (?P<ingredient>[a-zA-Z ]+)'
+regex_ = re.compile(pattern, re.DOTALL)  
+for ing in ingredients:
+    x = re.match(regex_, ing)
+    if x is not None:
+        print(x.groupdict())
+
+#get number of servings
+servings = recipe['Servings']
+
+
 
 with open(fref) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
     for row in csv_reader:
         print(row)
     
+
+
+
