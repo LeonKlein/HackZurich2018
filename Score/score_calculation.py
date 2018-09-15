8975fe3711bc75c5b59db876389e42fbcdeb121d
Marsll <Marsll@users.noreply.github.com>
2018-09-15 10:24:56 +0200

    Structure code more

--- Score/score_calculation.py
+++ Score/score_calculation.py
@@ -29,49 +29,44 @@ def extract_cost_table(fref_name):
     return costs_table
 
 
-recipes = read_scrapped_file(fname, region=(2, 9))
-costs_table = extract_cost_table(fref_name=fref)
-
-for recipe in recipes:
+def regex_matching(recipe):
     # get ingredients and their amount
     ingredients = recipe['Ingredients']
 
     quantities = (r"(?P<unit>cups|cup|ounce|tablespoons|tablespoon|teaspoons"
                   r"|teaspoon|pounds|pound|pints|pint|pinch|pinches|)")
     pattern = r'(?P<quantity>[0-9/ ]+)' \
-                + quantities    \
-                + r' (?P<ingredient>[a-zA-Z, ]+)'
+        + quantities    \
+        + r' (?P<ingredient>[a-zA-Z, ]+)'
     regex_ = re.compile(pattern, re.DOTALL)
     ingreds = []
     for ing in ingredients:
         x = re.match(regex_, ing)
         if x is not None:
             ingreds.append(x.groupdict())
-
-    # get number of servings
-    servings = recipe['Servings']
+    return ingreds
 
 
-    # total cost
+def calc_weight(amount):
+    """Calculate the amount in kg
+    """
+    conversion = {"cup": 0.15, "cups": 0.15, "tablespoon": 0.008,
+                  "tablespoons": 0.008, "ounce": 0.03, "ounces": 0.03, "pound": 0.45,
+                  "pounds": 0.45, "pint": 0.45, "pints": 0.45, "teaspoon": 0,
+                  "teaspoons": 0, "pinch": 0, "pinches": 0}
+
+    if amount['unit'] in list(conversion.keys()):
+        conversion_rate = conversion[amount['unit']]
+        print(amount)
+    else:
+        conversion_rate = 0
+        print(amount)
+    return float(Mixed(amount['quantity'])) * conversion_rate
+
+
+def calculate_score(ingredients):
     score = 0
-
-    def calc_weight(amount):
-        """Calculate the amount in kg
-        """
-        conversion = {"cup": 0.15, "cups": 0.15, "tablespoon": 0.008,
-                      "tablespoons": 0.008, "ounce": 0.03, "ounces": 0.03, "pound": 0.45,
-                      "pounds": 0.45, "pint": 0.45, "pints": 0.45, "teaspoon": 0,
-                      "teaspoons": 0, "pinch": 0, "pinches": 0}
-
-        if amount['unit'] in list(conversion.keys()):
-            conversion_rate = conversion[amount['unit']]
-            print(amount)
-        else:
-            conversion_rate = 0
-            print(amount)
-        return float(Mixed(amount['quantity'])) * conversion_rate
-
-    for ing in ingreds:
+    for ing in ingredients:
         if ing['ingredient'] in list(costs_table.keys()):
             score += costs_table[ing['ingredient']] * calc_weight(ing)
         else:
@@ -80,13 +75,24 @@ for recipe in recipes:
             value2 = np.around(np.random.random(), decimals=2) * 50
             value3 = np.around(np.random.random(), decimals=2)
             score += value1 * calc_weight(ing)
-            # print(calc_weight(ing))
 
             with open(fref, mode='a', newline='') as csv_file:
                 fieldnames = [ing['ingredient'], value1, value2, value3]
                 writer = csv.writer(csv_file, delimiter=';')
                 writer.writerow(fieldnames)
-    # print(score)
+    return score
 
-def calculate_score(recipe):
-    pass
+
+recipes = read_scrapped_file(fname, region=(2, 9))
+costs_table = extract_cost_table(fref_name=fref)
+
+# Loop over recipes
+# Calculate score fore every recipe
+for recipe in recipes:
+    ingreds = regex_matching(recipe)
+
+    # get number of servings
+    servings = recipe['Servings']
+
+    score = calculate_score(ingredients=ingreds)
+    print(score)
