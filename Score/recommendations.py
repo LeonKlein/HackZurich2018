f58764dc29d2c3c6babe3945d9cdeb36a7cd8c8d
Marsll <Marsll@users.noreply.github.com>
2018-09-15 11:34:24 +0200

    Start recommendations

---
+++ Score/recommendations.py
@@ -0,0 +1,27 @@
+import dataset
+from score_calculation import read_scrapped_file, extract_cost_table, regex_matching
+
+
+db_file = 'sqlite:///mydb.db'
+fname = "DataScrapper/tools/scrappedData.txt"
+fref = "Score/EnvironmentalData.csv"
+
+recipes = read_scrapped_file(fname, region=(1, 3))
+costs_table = extract_cost_table(fref_name=fref)
+
+
+def setup_db(db_file, recipes):
+    db = dataset.connect(db_file)
+    table = db['recipes']
+    table.drop()
+
+    for recipe in recipes:
+        ingreds = regex_matching(recipe)
+        #print(ingreds)
+        #table.insert(recipe["Ingredients"])
+
+    """ the_table = db['recipes'].all()
+    print([y for y in the_table]) """
+
+
+setup_db(db_file, recipes)
