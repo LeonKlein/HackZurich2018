56b501ec050905c28adfbb5863aae21d94b40c18
LeonKlein <leon.klein.2601@gmail.com>
2018-09-15 02:34:53 +0200

    Data fetched

--- Score/score_calculation.py
+++ Score/score_calculation.py
@@ -1,6 +1,7 @@
 import json
 import csv
 import re
+import numpy as np
 
 fname = "DataScrapper/tools/testData.txt"
 fref = "Score/EnvironmentalData.csv"
@@ -11,25 +12,35 @@ with open(fname) as f:
 
 #get ingredients and their amount
 ingredients = recipe['Ingredients']  
-print(ingredients)
 
 pattern = r'(?P<quantity>[0-9] [a-z]+) (?P<ingredient>[a-zA-Z ]+)'
-regex_ = re.compile(pattern, re.DOTALL)  
+regex_ = re.compile(pattern, re.DOTALL) 
+ingreds = [] 
 for ing in ingredients:
     x = re.match(regex_, ing)
     if x is not None:
-        print(x.groupdict())
+        ingreds.append(x.groupdict())
 
+print(ingreds)
 #get number of servings
 servings = recipe['Servings']
 
 
-
+#open Environmental costs of different food tpes
 with open(fref) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=';')
+    costs_table = {}
     for row in csv_reader:
-        print(row)
-    
+        costs_table[row[0]] = float(row[1])
+    print(costs_table)
 
+#total cost    
+score = 0
 
+for ing in ingreds:
+    if ing['ingredient'] in list(costs_table.keys()):
+        score += costs_table[ing['ingredient']]
+    else:
+        score += np.random.random() * 10
+print(score)
 
